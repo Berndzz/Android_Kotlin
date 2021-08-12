532ac12568b177e97a89f055624317766853e38a
Berndzz <tukanbernad@gmail.com>
2021-08-12 20:36:12 +0700

    tambah icon recView & tampilin di screen

--- Application_1/app/src/main/java/id/ac/ukdw/app_1/HomeActivity.kt
+++ Application_1/app/src/main/java/id/ac/ukdw/app_1/HomeActivity.kt
@@ -3,6 +3,8 @@ package id.ac.ukdw.app_1
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,17 @@ class HomeActivity :AppCompatActivity(){
         val username = intent.getStringExtra("username")
         val tvUsername = findViewById<TextView>(R.id.tvUsername)
         tvUsername.text = "Selamat Datang $username"
+
+        val listRec = ArrayList<List_recommended>()
+        listRec.add(List_recommended("Chatting","5 minutes",R.mipmap.ic_message))
+        listRec.add(List_recommended("Listens","2 minutes",R.mipmap.ic_headset))
+        listRec.add(List_recommended("Speak","3 minutes",R.mipmap.ic_mic))
+
+        val rvRec = findViewById<RecyclerView>(R.id.rvRecommended)
+        rvRec.layoutManager = LinearLayoutManager(this)
+
+        val listAdap = ListAdapter(listRec)
+        rvRec.adapter= listAdap
+
     }
 }
